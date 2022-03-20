4aef673f9fd910a3d4d40c8b654f46d539aceae8
Defirence <33593621+Defirence@users.noreply.github.com>
2022-03-20 11:08:35 +0200

    Update kube-check.sh

--- kube-check.sh
+++ kube-check.sh
@@ -1,2 +1,2 @@
-#/bin/bash
+#!/bin/bash
 kubectl get ns && kubectl get pods --all-namespaces && kubectl get deployments --all-namespaces && kubectl get services --all-namespaces && kubectl get clusterroles --all-namespaces
