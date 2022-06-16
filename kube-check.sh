bb476bffd143ce8cdfcee01b59a3aa4706bea52f
Defirence <33593621+Defirence@users.noreply.github.com>
2022-06-16 17:34:08 +0200

    Update script with formatting and general cleanup
    Signed-off-by: Defirence <33593621+Defirence@users.noreply.github.com>

--- kube-check.sh
+++ kube-check.sh
@@ -1,2 +1,26 @@
-#!/bin/bash
-kubectl get ns && kubectl get pods --all-namespaces && kubectl get deployments --all-namespaces && kubectl get services --all-namespaces && kubectl get clusterroles --all-namespaces
+#!/usr/bin/env/bash #!/usr/bin/env/zsh #!/usr/bin/env/sh
+
+CYAN='\033[0;36m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+
+printf "${CYAN}Retrieving Kubernetes namespaces...\n"
+echo "\n"
+kubectl get ns
+sleep 1
+echo "\n"
+printf "${CYAN}Retrieving ${YELLOW}pods ${GREEN}inside all namespaces...${CYAN}\n"
+kubectl get pods --all-namespaces
+sleep 1
+echo "\n"
+printf "${CYAN}Retrieving ${YELLOW}deployments ${GREEN}inside all namespaces...${CYAN}\n"
+kubectl get deployments --all-namespaces
+sleep 1
+echo "\n"
+printf "${CYAN}Retrieving ${YELLOW}services ${GREEN}inside all namespaces...${CYAN}\n"
+kubectl get services --all-namespaces
+sleep 1
+echo "\n"
+printf "${CYAN}Retrieving ${YELLOW}clusterroles ${GREEN}inside all namespaces...${CYAN}\n"
+kubectl get clusterroles --all-namespaces
+exit 0
